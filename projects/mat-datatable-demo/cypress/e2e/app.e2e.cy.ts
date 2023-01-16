c24567c21cfbf945fb3282c541d0caeaf9a61ea7
Bernhard Pottler <bpottler@nexgo.de>
2023-01-16 11:00:23 +0100

    fix: modify caption in demo page

--- projects/mat-datatable-demo/cypress/e2e/app.e2e.cy.ts
+++ projects/mat-datatable-demo/cypress/e2e/app.e2e.cy.ts
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/');
-    cy.contains('cypress:demo:open');
+    cy.contains('Mat-Datatable-Demo');
   });
 });
