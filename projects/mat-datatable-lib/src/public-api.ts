be3f276de94853eefed10c61f2a406e5aa04a389
Bernhard Pottler <bpottler@nexgo.de>
2023-02-19 06:23:04 +0100

    build: add interface definitions to public-api

--- projects/mat-datatable-lib/src/public-api.ts
+++ projects/mat-datatable-lib/src/public-api.ts
@@ -4,3 +4,6 @@
 
 export * from './lib/mat-datatable.component';
 export * from './lib/mat-datatable.module';
+export * from './interfaces/datatable-datasource.class';
+export * from './interfaces/datatable-column-definition.interface';
+export * from './interfaces/datatable-sort-definition.interface';
