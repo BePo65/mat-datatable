9e8040484365691cb5e6870a2c25863ca4b0c502
Bernhard Pottler <bpottler@nexgo.de>
2023-02-27 11:59:28 +0100

    build: add public-api.ts and index.ts to datatable-sort

---
+++ projects/mat-datatable-lib/src/directives/datatable-sort/public-api.ts
@@ -0,0 +1,14 @@
+/*
+ * Public API Surface of mat-multi-sort
+ */
+
+export {
+  ArrowViewState,
+  ArrowViewStateTransition,
+  matSortAnimations,
+  MatSortHeaderIntl,
+  SortDirection
+} from '@angular/material/sort';
+export * from './mat-multi-sort.directive';
+export * from './mat-multi-sort-header.component';
+export * from './mat-multi-sort.module';
