941ac46f15e45aec7a055e96811e831b9dc9cdf1
Bernhard Pottler <bpottler@nexgo.de>
2023-04-20 06:53:39 +0200

    fix: remove unused exports from public-api of mat-datatable-lib

--- projects/mat-datatable-lib/src/lib/public-api.ts
+++ projects/mat-datatable-lib/src/lib/public-api.ts
@@ -2,20 +2,7 @@
  * Public API Surface of mat-multi-sort
  */
 
-// TODO do we need all of these exports?
-// TODO do we need the exports from datatable-sort??
-// TODO add exports from datatable-sort/testing
-
-export {
-  ArrowViewState,
-  ArrowViewStateTransition,
-  matSortAnimations,
-  MatSortable,
-  MatSortHeaderIntl,
-  Sort,
-  SortDirection,
-  SortHeaderArrowPosition
-} from '@angular/material/sort';
+export { Sort } from '@angular/material/sort';
 
 export * from './mat-datatable.component';
 export * from './mat-datatable.module';
