51fd4b79d10029831e1296a20144973f8e520399
Bernhard Pottler <bpottler@nexgo.de>
2023-04-22 07:18:02 +0200

    refactor: create public-api for lib and testing in project root

--- projects/mat-datatable-lib/src/lib/testing/public-api.ts
+++ projects/mat-datatable-lib/src/lib/testing/public-api.ts
@@ -6,6 +6,3 @@ export * from './mat-datatable-table-harness';
 export * from './mat-datatable-row-harness';
 export * from './mat-datatable-cell-harness';
 export * from './mat-datatable-harness-filters';
-// TODO should we move the 'directives' and the 'interfaces' directory to 'lib'?
-// TODO do mat-sort and mat-datatable testings overlap when we come to selecting columns (by label)?
-export * from '../../directives/datatable-sort/testing';
---
+++ projects/mat-datatable-lib/src/testing/public-api.ts
@@ -0,0 +1,6 @@
+/**
+ * Public API Surface of mat-datatable testing harness
+ */
+
+export * from '../lib/testing';
+export * from '../directives/datatable-sort/testing';
