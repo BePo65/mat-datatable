ddfcba7e2d0573d79b47ca2f1f3c04fc1be7f8f0
Bernhard Pottler <bpottler@nexgo.de>
2024-05-01 07:06:00 +0200

    docs(demo): add todo issues

--- projects/mat-datatable-demo/src/main.ts
+++ projects/mat-datatable-demo/src/main.ts
@@ -29,3 +29,12 @@ bootstrapApplication(AppComponent, {
   ]
 })
   .catch(err => console.error(err));
+
+// TODO 'withInterceptorsFromDi' see following comment
+/**
+ * Includes class-based interceptors configured using a multi-provider in the current injector into
+ * the configured `HttpClient` instance.
+ *
+ * Prefer `withInterceptors` and functional interceptors instead, as support for DI-provided
+ * interceptors may be phased out in a later release.
+ */
